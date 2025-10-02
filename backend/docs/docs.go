8c9912f430f6f943b8d64b1b5aed56d7acc221ac
Demid <demid.shumakher@gmail.com>
2025-10-02 19:32:58 +0300

    fix

--- backend/docs/docs.go
+++ backend/docs/docs.go
@@ -209,7 +209,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "http://164.90.219.207:8080",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "API",
